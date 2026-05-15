ee070801fffd96b157b6624b681aac1b1a16a029
jhs512 <jangka512@gmail.com>
2026-05-15 15:07:03 +0900

    026-1

--- src/main/java/com/back/domain/post/post/controller/PostController.java
+++ src/main/java/com/back/domain/post/post/controller/PostController.java
@@ -43,7 +43,7 @@ public class PostController {
         return "post/post/write";
     }
 
-    @PostMapping("/posts/doWrite")
+    @PostMapping("/posts/write")
     @Transactional
     public String write(
             @ModelAttribute("form") @Valid WriteForm form,
